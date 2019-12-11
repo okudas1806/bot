22241ab3f1a875e3b616a3757432555f3a84ba86
Maxence Dubois <maxence1806@free.fr>
2019-12-11 21:42:54 +0100

    maj 11/12/19 1.1

--- main.js
+++ main.js
@@ -1,15 +1,16 @@
 const { Client } = require("discord.js");
 const { TOKEN, PREFIX } = require("./config");
 const client = new Client({ disableEveryone: true });
-const prefix = "test!";
-
-client.on("ready", () => {
-  console.log("je suis pret!");
-});
 
 client.on("message", msg => {
-  if (msg.content.startsWith(`${PREFIX}ping`)) msg.channel.send("pong!");
-  if (msg.content.startsWith(`${PREFIX}pong`)) msg.channel.send("ping!");
+  if (msg.author.bot) return;
+  const args = msg.content.split(/ +/g);
+  const cmd = args.shift().toLowerCase();
+  if (cmd === `${PREFIX}ping`) msg.channel.send("pong!");
+  if (cmd === `${PREFIX}pong`) msg.channel.send("ping!");
+  if (cmd === `${PREFIX}lol`) msg.channel.send("test!lol");
 });
 
 client.login(TOKEN);
+
+client.on("ready", () => console.log("je suis pret!"));
