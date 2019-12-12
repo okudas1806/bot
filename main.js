f6821a6afd49bf77aa0916d3f28c4b43159ccbc0
Maxence Dubois <maxence1806@free.fr>
2019-12-12 21:48:21 +0100

    maj 12/12/19

--- main.js
+++ main.js
@@ -9,6 +9,30 @@ client.on("message", msg => {
   if (cmd === `${PREFIX}ping`) msg.channel.send("pong!");
   if (cmd === `${PREFIX}pong`) msg.channel.send("ping!");
   if (cmd === `${PREFIX}lol`) msg.channel.send("test!lol");
+  if (cmd === `${PREFIX}repeat`) {
+    msg.channel.send(args.join(" "));
+    msg
+      .delete({ timeout: 3 })
+      .then(console.log(`Message suprimé: ${msg.content}`));
+  }
+  if (cmd === `${PREFIX}role`) {
+    const channel = client.channels.find(r => r.name === "logs");
+    const role = msg.guild.roles.find(r => r.name === args[0]);
+    if (!role) return msg.channel.send("Ce role n'existe pas!");
+    if (msg.member.roles.find(r => r.name === args[0])) {
+      msg.member.roles.remove(role);
+      channel.send(`le role ${role} à bien était enlevé à ${msg.author}.`);
+      msg
+        .delete({ timeout: 3 })
+        .then(console.log(`Message suprimé: ${msg.content}`));
+    } else {
+      msg.member.roles.add(role);
+      channel.send(`le role ${role} à bien était ajouté à ${msg.author}.`);
+      msg
+        .delete({ timeout: 3 })
+        .then(console.log(`Message suprimé: ${msg.content}`));
+    }
+  }
 });
 
 client.on("guildMemberAdd", member => {
